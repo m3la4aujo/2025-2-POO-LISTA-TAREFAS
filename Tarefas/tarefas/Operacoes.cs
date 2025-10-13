6b029ed10f68942684e624955e8f76b70cd31476
m3la4aujo <167586116+m3la4aujo@users.noreply.github.com>
2025-10-13 23:49:36 +0000

    feat: Lista_tarefas

---
+++ Tarefas/tarefas/Operacoes.cs
@@ -0,0 +1,22 @@
+public class Operacoes
+{
+    public int Criar(Tarefa tarefa)
+    {
+        return 0;
+    }
+    public Tarefa Buscar(int id)
+    {
+        return null;
+    }
+    public List<Tarefa> Listar()
+    {
+        return Array.Empty<Tarefa>();
+    }
+    public void Alterar(Tarefa tarefa)
+    {
+
+    }
+    public void Excluir(int id)
+    {
+}
+}
---
+++ Tarefas/tarefas/Tarefa.cs
@@ -0,0 +1,9 @@
+public class Tarefa
+{
+    public int id {get; set;}
+    public string nome {get; set;}
+    public string descricao {get; set;}
+    public DateTime dataCriacao {get; set;}
+    public int status {get; set;}
+    public DateTime dataExecucao {get; set;}
+}
