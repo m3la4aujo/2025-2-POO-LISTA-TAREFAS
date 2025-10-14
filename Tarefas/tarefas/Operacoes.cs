18b768d1174f718c32b838edcb0a32a33490cc65
m3la4aujo <167586116+m3la4aujo@users.noreply.github.com>
2025-10-14 00:51:19 +0000

    feat:Lista_Tarefas

--- Tarefas/tarefas/Operacoes.cs
+++ Tarefas/tarefas/Operacoes.cs
@@ -1,13 +1,15 @@
+using MySql.Data.MySqlClient;
+
 public class Operacoes
 {
-    private string connectionString = @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;passsword=S3nh4!F0rt3;database=user_poo;";
+    private string connectionString = @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
     public int Criar(Tarefa tarefa)
     {
         using(var conexao = new MySqlConnection(connectionString))
         {
             conexao.Open();
-            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao)
-                           VALUES (@nome, @descricao, @dataCriacao, @status, @dataExeucao);
+            string sql = @"INSERT INTO tarefas (nome, descricao, dataCriacao, status, dataExecucao)
+                           VALUES (@nome, @descricao, @dataCriacao, @status, @dataExecucao);
                            SELECT LAST_INSERT_ID();";
             using (var cmd = new MySqlCommand(sql, conexao))
             {
@@ -17,7 +19,7 @@ public class Operacoes
                 cmd.Parameters.AddWithValue("@status", tarefa.Status);
                 cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
 
-                return convert.ToInt32(cmd.ExecuteScalar());
+                return Convert.ToInt32(cmd.ExecuteScalar());
             }
         }
     }
@@ -29,14 +31,16 @@ public class Operacoes
 
     public List<Tarefa> Listar()
     {
-        return Array.Empty<Tarefa>():
-    {
+        return Array.Empty<Tarefa>();
+        
+    }
 
     public void Alterar(Tarefa tarefa)
     {
 
     }
     public void Excluir(int id)
-}
-}
+    {
+
+    }
 }
